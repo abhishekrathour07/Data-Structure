6d49a6d13ed116e93bd7e7feab467a4663faf6f1
abhishekrathour07 <singhrathourabhishek7@gmail.com>
2023-03-14 00:28:23 +0530

    sreach technique

--- 1_2lineraSrearch.c
+++ 1_2lineraSrearch.c
@@ -1,7 +1,7 @@
 #include<stdio.h>         
 
 void main(){
-    int search,i,a[20],size,c;
+    int search,i,a[20],size,c=0;
     printf("Enter the size of an array");
     scanf("%d",&size);
     printf("Enter the element in the array");
